7bc2ff2b0d09168be9cffdfe83e0b1ce03e5e297
Egidio Cespedes <egidio@localhost.localdomain>
2025-10-19 00:35:38 -0300

    feat: sort

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ path
 
 folder_test = os.listdir(folder + '/' + subfolder[0] + '/' + 'Test') # las carpetas que contiene
 folder_train = os.listdir(folder + '/' + subfolder[0] + '/' + 'Train')
-folder_train
+folder_train.sort()
 
 subfolder[0]
 
@@ -31,6 +31,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+for x in [0,1,2,3,4,5,6,7,8]:
+	print(files[x])
+
+files.sort()
+print('---------------')
+for x in [0,1,2,3,4,5,6,7,8]:
+	print(files[x])
+
 # leer imagen con openCV, en este caso el archivo se lee en BGR
 im1 = cv2.imread(folder + '/' + subfolder[0] + '/' + 'Train' + '/' + 'melanoma/' + files[5])
 # por lo cual debemos convertirla a RGB
@@ -39,6 +47,8 @@ im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
 
 # leer imagen con PIL, en este caso el archivo se lee en RGB
 im2 = np.array(PIL.Image.open(folder + '/' + subfolder[0] + '/' + 'Train' + '/' + 'melanoma/' + files[5]))
+
+
 
 fig, ax = plt.subplots(1,2,figsize=(20,8))
 ax[0].imshow(im1)
